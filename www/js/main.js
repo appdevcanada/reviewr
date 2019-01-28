c386a1198d57513a8f9e7b8e2803fd66fa559ac6
mora0199@algonquinlive.com <43067488+LuisHMSouza@users.noreply.github.com>
2019-01-27 21:39:18 -0500

    take screen

--- www/js/main.js
+++ www/js/main.js
@@ -9,13 +9,14 @@ let app = {
         let isReady = (window.hasOwnProperty("cordova"))?'deviceready':'DOMContentLoaded';
         document.addEventListener(isReady, ()=>{
             app.pages = document.querySelectorAll('.page');
-            app.pages[1].classList.add('active');
-            app.pages.forEach( page => {
+            app.pages[0].classList.add('active');
+            app.getLSData();
+            /*             app.pages.forEach( page => {
                 page.querySelector('h1').addEventListener('click', app.navigate);
             });
-        });
+ */        });
     },
-    navigate: function(ev){
+/*     navigate: function(ev){
         ev.preventDefault();
         let tapped = ev.currentTarget; //the clicked h1
         console.log(tapped);
@@ -23,6 +24,36 @@ let app = {
         //hide the only active page
         let target = tapped.getAttribute('data-target');
         document.getElementById(target).classList.add('active');
-    }
+        app.getLSData();
+    },
+ */    getLSData: function(ev){
+        // First see if data exists in local storage
+        if (localStorage.length > 0) {
+            // Keys exist in local storage so let's get their values
+            localStorage.forEach( key => {
+                let idTag = JSON.parse(localStorage.getItem(idKey));
+                let titleTag = document.getElementById("titletag");
+                let ratingTag = document.getElementById("ratingtag");
+                let imgTag = document.getElementById("imgtag");
+                
+                titleTag.value = JSON.parse(localStorage.getItem(titleKey));
+                ratingTag.value = JSON.parse(localStorage.getItem(ratingKey));
+                imgTag.value = JSON.parse(localStorage.getItem(imgKey));
+    
+            })
+
+        } else {
+            //data doesn't exist in local storage
+            let slider = document.getElementById("sliderating");
+            let output = document.getElementById("lblvalue");
+            output.innerHTML = slider.value;
+            slider.oninput = function() {
+                output.innerHTML = this.value;
+            }
+            app.pages[0].classList.remove('active');
+            app.pages[2].classList.add('active');
+            console.log("Data not saved in Local Storage");
+            }   
+        }   
 };
 app.init();
